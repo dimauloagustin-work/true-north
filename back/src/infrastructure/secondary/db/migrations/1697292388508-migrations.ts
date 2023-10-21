45702ab0c27ee8b1553708345b17bb42bb75d94e
Agustin, Di Maulo <dimauloagustin@gmail.com>
2023-10-20 23:47:38 -0300

    fix migration

--- back/src/infrastructure/secondary/db/migrations/1697292388508-migrations.ts
+++ back/src/infrastructure/secondary/db/migrations/1697292388508-migrations.ts
@@ -4,12 +4,10 @@ export class Migrations1697292388508 implements MigrationInterface {
     name = 'Migrations1697292388508'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`record\` ADD \`active\` tinyint NOT NULL DEFAULT TRUE`);
         await queryRunner.query(`INSERT INTO main.\`user\` (username, password, status) VALUES(\`test@test.com\`, \`$2b$10$WKpQm7UUKA8lmw.4.Qt0xeqlHhCOZye5sEJY888.CWp0rveHsbyju\`, 1)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`record\` DROP COLUMN \`active\``);
         await queryRunner.query(`delete from main.user where user.username = \`test@test.com\``);
     }
 
