b6fc272de77fe55ec62a73567de62812e9199394
Agustin, Di Maulo <dimauloagustin@gmail.com>
2023-10-20 23:57:28 -0300

    fix

--- back/src/infrastructure/secondary/db/migrations/1697292388508-migrations.ts
+++ back/src/infrastructure/secondary/db/migrations/1697292388508-migrations.ts
@@ -4,7 +4,7 @@ export class Migrations1697292388508 implements MigrationInterface {
     name = 'Migrations1697292388508'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`INSERT INTO main.\`user\` (username, password, status) VALUES(\`test@test.com\`, \`$2b$10$WKpQm7UUKA8lmw.4.Qt0xeqlHhCOZye5sEJY888.CWp0rveHsbyju\`, 1)`);
+        await queryRunner.query(`INSERT INTO main.\`user\` (username, password, status) VALUES('test@test.com', '$2b$10$WKpQm7UUKA8lmw.4.Qt0xeqlHhCOZye5sEJY888.CWp0rveHsbyju', 1)`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
